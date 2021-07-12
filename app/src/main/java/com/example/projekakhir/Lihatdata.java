6255b297f04719b368c14525d661c554bc1caadc
Karmilatul Hayyat <63487509+KarmilatulHayyat@users.noreply.github.com>
2021-07-13 00:01:03 +0700

    Update Lihatdata.java
    comment

--- app/src/main/java/com/example/projekakhir/Lihatdata.java
+++ app/src/main/java/com/example/projekakhir/Lihatdata.java
@@ -1,4 +1,17 @@
 package com.example.projekakhir;
 
-public class Lihatdata {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lihatdata extends AppCompatActivity {
+    @Override //subclass untuk mengubah variabel. Mengubah di sini bisa berarti menggantikan perintah,
+                // membuat perintah baru, mengembangkan perintah, dan memodifikasi perintah.
+
+
+    protected void onCreate(Bundle savedInstanceState) {        //penjelasan sama seperti di class MainActivity.
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihatdata);
+    }
 }
+
